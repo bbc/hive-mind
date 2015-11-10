443ed07010066dced3dd306fecaf5c02915cd075
Jon Wilson <jon.wilson01@bbc.co.uk>
2015-11-10 13:52:17 +0000

    Added omniauth support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,52 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def auth_callback
+    current_user
+    omniauth_origin = session[:omniauth_origin]
+    session.delete(:onmiauth_origin)
+    redirect_to omniauth_origin || '/'
+  end
+
+  private
+
+  def current_user
+    if @current_user.nil?
+      if session[:user_id]
+        @current_user = User.find(session[:user_id])
+      elsif omniauth_credentials
+        credentials = omniauth_credentials
+        @current_user = User.find_or_create_from_omniauth_hash(credentials)
+      end
+
+      if @current_user.nil?
+        if Rails.application.config.force_authentication
+          unless session[:omniauth_origin]
+            session[:omniauth_origin] = request.original_url
+            redirect_to('/auth/' + Rails.application.config.default_omniauth_provider.to_s)
+          end
+        end
+      end
+
+      session[:user_id] = @current_user.id if @current_user
+    end
+
+    @current_user || User.anonymous_user
+  end
+
+  def omniauth_credentials
+    if omniauth_hash = request.env['omniauth.auth']
+      {
+          provider:   omniauth_hash['provider'],
+          uid:        omniauth_hash['uid'],
+          email:      omniauth_hash['info']['email'],
+          name:       omniauth_hash['info']['name']
+      }
+    else
+      nil
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109102458) do
+ActiveRecord::Schema.define(version: 20151109214132) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
@@ -56,5 +56,12 @@ ActiveRecord::Schema.define(version: 20151109102458) do
   end
 
   add_index "models", ["brand_id"], name: "index_models_on_brand_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "provider"
+    t.string "uid"
+  end
 
 end
