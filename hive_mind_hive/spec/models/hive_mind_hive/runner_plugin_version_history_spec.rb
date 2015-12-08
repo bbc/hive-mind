dbb08aff5ebd9156f2c09a94b1b751cf1e7df0d2
Joe Haig <joe.haig@bbc.co.uk>
2015-12-08 10:36:49 +0000

    Remove empty spec files

--- hive_mind_hive/spec/models/hive_mind_hive/runner_plugin_version_history_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-module HiveMindHive
-  RSpec.describe RunnerPluginVersionHistory, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-end
--- hive_mind_hive/spec/models/hive_mind_hive/runner_plugin_version_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-module HiveMindHive
-  RSpec.describe RunnerPluginVersion, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-end
