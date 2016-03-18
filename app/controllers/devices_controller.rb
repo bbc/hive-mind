94993727664bee7b330240b58da8317afab48fba
Joe Haig <joe.haig@bbc.co.uk>
2016-03-18 10:03:12 +0000

    Fix some problems from a bad merge

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -4,7 +4,7 @@ class DevicesController < ApplicationController
   # GET /devices
   # GET /devices.json
   def index
-    @devices = Device.all.group_by { |d| d.model.device_type }
+    @devices = Device.all.group_by { |d| d.model && d.model.device_type }
   end
   
   def search
