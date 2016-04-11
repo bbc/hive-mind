ad4de6628a5e685cff510be0fb837a91b1aa9606
David Buckhurst <david.buckhurst@bbc.co.uk>
2016-04-11 22:20:47 +0100

    Default to port 3001

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# Override rails defaults
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '0.0.0.0', Port: 3001)
+    end
+  end
+end
