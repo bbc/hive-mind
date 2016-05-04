ad942dc9a206752372bb12f4e91b5607b022981b
Joe Haig <joe.haig@bbc.co.uk>
2016-05-04 10:30:25 +0100

    Remove commented code

--- app/helpers/hive_helper.rb
+++ app/helpers/hive_helper.rb
@@ -1,10 +1,5 @@
 module HiveHelper
   def all_hives
     Device.joins(model: :device_type).where('device_types.classification = ?', 'Hive')
-    #Device.where(
-    #  model: Model.where(
-    #    device_type: DeviceType.where(classification: 'Hive')
-    #  )
-    #)
   end
 end
