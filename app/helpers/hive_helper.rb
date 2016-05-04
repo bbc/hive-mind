523374a8fb3148e8af0323f63f07518d922532f0
Joe Haig <joe.haig@bbc.co.uk>
2016-05-04 09:36:43 +0100

    Improve all_hives method

--- app/helpers/hive_helper.rb
+++ app/helpers/hive_helper.rb
@@ -1,9 +1,10 @@
 module HiveHelper
   def all_hives
-    Device.where(
-      model: Model.where(
-        device_type: DeviceType.where(classification: 'Hive')
-      )
-    )
+    Device.joins(model: :device_type).where('device_types.classification = ?', 'Hive')
+    #Device.where(
+    #  model: Model.where(
+    #    device_type: DeviceType.where(classification: 'Hive')
+    #  )
+    #)
   end
 end
