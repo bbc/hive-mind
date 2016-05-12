d617a1e92dd7883219de8ec2456bd7eb95c15802
Jon Wilson <jon.wilson01@bbc.co.uk>
2016-05-12 12:39:38 +0100

    Added in chamber

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,7 @@
-Paperclip::Attachment.default_options[:storage] = ENV['ATTACHMENT_STORAGE']
+Paperclip::Attachment.default_options[:storage] = Chamber.env.attachment.storage
 
 if Chamber.env.attachment.storage == 's3'
-  s3_config = { s3_credentials: { bucket: ENV['ATTACHMENT_S3_BUCKET'] }, s3_permissions: { original: :private } }
+  s3_config = { s3_credentials: { bucket: Chamber.env.attachment.s3_bucket }, s3_permissions: { original: :private } }
   Paperclip::Attachment.default_options.merge!(s3_config)
 end
 
