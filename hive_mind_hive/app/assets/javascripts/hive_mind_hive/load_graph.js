ce2db3198a6349fc78331d83fe90781f8ca57a43
Joe Haig <josephhaig@gmail.com>
2016-07-05 15:45:35 +0100

    Allow for multiple graphs

--- hive_mind_hive/app/assets/javascripts/hive_mind_hive/load_graph.js
+++ hive_mind_hive/app/assets/javascripts/hive_mind_hive/load_graph.js
@@ -1,9 +1,17 @@
-load_graph = function(svg_class, dataset, cpu_count, load_warn, load_err, width, height) {
+initialize_graph = function(svg_class, width, height) {
   var svg = d3.select(svg_class);
-  var bar_width = width / dataset.length;
   svg
     .attr('width', width + 'px')
     .attr('height', height + 'px');
+};
+
+load_graph = function(svg_class, dataset, cpu_count, load_warn, load_err) {
+  var svg = d3.select(svg_class);
+
+  var width = svg.style('width').replace('px', '');
+  var height = svg.style('height').replace('px', '');
+
+  var bar_width = width / dataset.length;
   svg.selectAll("rect")
     .remove();
   svg.selectAll("rect")
@@ -23,4 +31,36 @@ load_graph = function(svg_class, dataset, cpu_count, load_warn, load_err, width,
         return 'green'
       }
     });
-}
+};
+
+updateGraph = function(device_id, nproc, load_warn, load_err) {
+  data_url = "/api/device_statistics/stats/" + device_id + "/Load average/60";
+  $.ajax({
+    type: 'GET',
+    url: data_url + '#' + (Math.floor(Math.random() * 1000000) + 1),
+    success: function(data) {
+      load_graph(
+        "#load_chart_" + device_id,
+        data['data'],
+        nproc,
+        load_warn,
+        load_err
+      );
+      currentLoad =  parseFloat(data['data'][data['data'].length - 1]);
+      loadBox = document.getElementById("stat_box_" + device_id);
+      if (currentLoad/nproc < load_warn ) {
+        loadBox.className = 'label label-success';
+      } else if (currentLoad/nproc < load_err ) {
+        loadBox.className = 'label label-warning';
+      } else {
+        loadBox.className = 'label label-danger';
+      }
+
+      document.getElementById("stat_figure_" + device_id).innerHTML = parseFloat(data['data'][data['data'].length - 1]).toFixed(2);
+      setTimeout(updateGraph, 5000, device_id, nproc, load_warn, load_err);
+    },
+  }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+    loadBox = document.getElementById("stat_box_" + device_id).className = 'label label-default';
+    setTimeout(updateGraph, 5000, device_id, nproc, load_warn, load_err);
+  });
+};
